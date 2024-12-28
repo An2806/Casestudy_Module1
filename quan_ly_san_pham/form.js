16324555cdced84db06319fe60ceb1bf9c7e5ae3
An2806 <taman1842@gmail.com>
2024-12-29 00:34:38 +0700

    mess

--- quan_ly_san_pham/form.js
+++ quan_ly_san_pham/form.js
@@ -6,13 +6,13 @@
 //     console.log(document.getElementById("gia").value);
 // }
 class SanPham {
-    constructor(maSanPham, tenSanPham, kichThuoc, mauSac, gia,hanhDong) {
+    constructor(maSanPham, tenSanPham, kichThuoc, mauSac, gia) {
         this.maSanPham = maSanPham;
         this.tenSanPham = tenSanPham;
         this.kichThuoc = kichThuoc;
         this.mauSac = mauSac;
         this.gia = gia;
-        this.hanhDong = hanhDong;
+
     }
 }
 
@@ -22,35 +22,58 @@ danhSachSanPham.push(new SanPham('001', 'Áo cộc', 'M', 'White', 90_000));
 danhSachSanPham.push(new SanPham('002', 'Quần dài','S', 'Black', 50_000));
 
 
-displayProduct();
+
 
 function displayProduct() {
     let html = '';
     for (let i = 0; i < danhSachSanPham.length; i++) {
         html += `<tr>
-          <td>${danhSachSanPham[i].maSanPham}<button onclick="deleteProduct(${i})">Delete</button></td>
+          <td>${danhSachSanPham[i].maSanPham}</td>
           <td>${danhSachSanPham[i].tenSanPham}</td>
           <td>${danhSachSanPham[i].kichThuoc}</td>
           <td>${danhSachSanPham[i].mauSac}</td>
           <td>${danhSachSanPham[i].gia}</td>
-          <td>${danhSachSanPham[i].hanhDong}</td>
-        </tr>`;
+          <td><button onclick="deleteProduct(${i})">Delete</button>
+          <button onclic="upDateProduct(${i})">UpDate</button>
+          <button onclick="editProduct(${i})">Edit</button></td>
+          
+      </tr>`;
     }
     document.getElementById("tbody").innerHTML = html;
 }
 
-function deleteProduct() {
+function deleteProduct(i) {
+    if (confirm("Bạn có chắc chắn muốn xóa sản phẩm này?")) {
+        danhSachSanPham.splice(i, 1);
+        displayProduct()
+    }
 
-
+function upDateProduct(i){
+    document.getElementById("maSanPham").value = danhSachSanPham.maSanPham;
+    document.getElementById("tenSanPham").value = danhSachSanPham.tenSanPham;
+    document.getElementById("kichThuoc").value = danhSachSanPham.kichThuoc;
+    document.getElementById("mauSac").value = danhSachSanPham.mauSac;
+    document.getElementById("gia").value = danhSachSanPham.gia;
+displayProduct()
+}
 
 }
 
-function createProduct() {
-    let maSanPham = document.getElementById("maSanPham").value;
-    let tenSanPham = document.getElementById("tenSanPham").value;
-    let kichThuoc = document.getElementById("kichThuoc").value;
-    let mauSac = document.getElementById("mauSac").value;
-    let gia = document.getElementById("gia").value;
-    danhSachSanPham.push(new SanPham(maSanPham, tenSanPham, kichThuoc, mauSac, gia));
-    displayProduct()
+    function editProduct(i){
+
+    }
+
+    function createProduct() {
+            let maSanPham = document.getElementById("maSanPham").value;
+            let tenSanPham = document.getElementById("tenSanPham").value;
+            let kichThuoc = document.getElementById("kichThuoc").value;
+            let mauSac = document.getElementById("mauSac").value;
+            let gia = document.getElementById("gia").value;
+            danhSachSanPham.push(new SanPham(maSanPham, tenSanPham, kichThuoc, mauSac, gia));
+            displayProduct()
+        }
+    }
+
 }
+
+displayProduct();
